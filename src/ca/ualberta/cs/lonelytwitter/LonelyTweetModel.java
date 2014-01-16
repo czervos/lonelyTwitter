5a9c50a879cdc23bde98582d0c6b373dfd52a3ce
czervos <czervos@ualberta.ca>
2014-01-16 14:57:47 -0700

    added more to lonelytweetmodel

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -10,6 +10,22 @@ public class LonelyTweetModel
 	private String text;
 	private Date timestamp; // referring to java.util.Date
 	
+	public LonelyTweetModel(String text) // also a constructor, it will choose which to use based on the type used, or in this case, how many arguments you provide
+	{
+
+		super();
+		this.text = text;
+		timestamp = new Date(); // refers to the class attribute timestamp (here since there is only one timestamp, the this. is implied); new creates a new instance of the object; this is calling the default date constructor
+	}
+
+	public LonelyTweetModel(String text, Date timestamp) // constructors have same name as the class
+	{
+
+		super(); // refers to the parent (super) class 
+		this.text = text;
+		this.timestamp = timestamp;
+	}
+
 	public String getText()
 	{
 	
@@ -19,7 +35,7 @@ public class LonelyTweetModel
 	public void setText(String text)
 	{
 	
-		this.text = text; // this is a keyword that refers to the current object here text refers to the argument string text, this.text refers to private string text above
+		this.text = text; // this is a keyword that refers to the current object here text refers to the argument string text, this.text refers to private string text above on line 10
 	}
 	
 	public Date getTimestamp()
